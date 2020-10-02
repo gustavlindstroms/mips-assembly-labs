59772a627e5da807b79c0b5d4f0227d55ffae851
Gustav Lindström <gu0843li-s@student.lu.se>
2020-10-02 18:06:13 +0200

    fixed tabs

--- lab-01/camelCase.asm
+++ lab-01/camelCase.asm
@@ -10,35 +10,37 @@ camelcase:
 	addi $t3, $zero, 1 # set boolean to true
 	
 	camelcase_loop:
-	lb $t2, string($t0) # load first char if string to $t2
-	beqz $t2, camelcase_done #if char == null go to done
-	beq $t3, 0, lowercase #if false go to caps
-	beq $t3, 1, caps # if true go to lowercase
-	back:
-	sb $t2, string($t0) # store the modified char
-	addi $t0, $t0, 1 #i++
-	j camelcase_loop
+		lb $t2, string($t0) # load first char if string to $t2
+		beqz $t2, camelcase_done #if char == null go to done
 	
-	caps: 
-	blt $t2, '[', caps_done 	# Skip char if ASCII value in $T0 is less than 97 
-	subi $t2, $t2, 32
-	j caps_done
+		beq $t3, 0, lowercase #if false go to caps
+		beq $t3, 1, caps # if true go to lowercase
 	
-	caps_done:
-	addi $t3, $zero, 0
-	j back
+		back: # point to jump back from subroutine
+		sb $t2, string($t0) # store the modified char
+		addi $t0, $t0, 1 #i++
+		j camelcase_loop # jump to top of loop
 	
-	lowercase:
-	bgt $t2, '`', lowercase_done
-	addi $t2, $t2, 32
-	j lowercase_done
+			caps: 
+			blt $t2, '[', caps_done 	# skip char if already uppercase 
+			subi $t2, $t2, 32 # subtract 32 to capitalize
+			j caps_done
 	
-	lowercase_done:
-	addi $t3, $zero, 1
-	j back
+				caps_done:
+				addi $t3, $zero, 0 # boolean = false (next letter should not be capitalized)
+				j back
+	
+			lowercase:
+			bgt $t2, '`', lowercase_done # skip if aready lowercase
+			addi $t2, $t2, 32 #add 32 to make it lowercase
+			j lowercase_done
+	
+				lowercase_done:
+				addi $t3, $zero, 1 #boolean = true
+				j back # jump back to method
 	
 	camelcase_done:
-	jr $ra
+	jr $ra # jump return to main
 	
 main:
 	la $a1, string
